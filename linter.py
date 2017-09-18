5652385329f6db668402d0eb1c3a92455fb780b6
Jan T. Sott <jan@idleberg.com>
2017-09-18 22:17:23 +0200

    minor fixes

--- linter.py
+++ linter.py
@@ -10,25 +10,20 @@
 # License: MIT
 #
 
-
 """This module exports the Iscc plugin class."""
 
 from SublimeLinter.lint import Linter, util
-from sys import platform
 
 
 class Iscc(Linter):
 
     """Provides an interface to the ISCC executable."""
 
-    if platform == 'win32':
-        cmd = ('ISCC.exe', '/Q', '/O-', '@')
-        syntax = 'inno setup'
-        regex = (
-            r'^Error on line (?P<line>\d+) in (?P<file>.*\.iss): (?P<message>.+)$'
-        )
-        multiline = True
-        error_stream = util.STREAM_STDERR
-        line_col_base = (1, 1)
-    else
-        print("This linter is only available on Windows")
+    cmd = ('ISCC.exe', '/Q', '/O-', '@')
+    syntax = 'inno setup'
+    regex = (
+        r'^Error on line (?P<line>\d+) in (?P<file>.*\.iss): (?P<message>.+)$'
+    )
+    multiline = True
+    error_stream = util.STREAM_STDERR
+    line_col_base = (1, 1)
